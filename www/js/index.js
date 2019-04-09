c5765a13489aa883613eb066d185eb36e4444b13
Teet Triisa <ttriisa@tlu.ee>
2019-04-09 19:52:46 +0300

    Add previous/next page functionality, highlight current chapter

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,6 @@
 //create our angularjs app
 var bytutorialEbookApp = angular.module('bytutorialEbookApp', ['ngRoute']);
-
+let currentRoute = ''
 //once the html is ready we attach the angularjs directive app into the body
 angular.element(document).ready(function () {
     if (window.cordova) {
@@ -35,14 +35,55 @@ bytutorialEbookApp.controller('chapterController', ['$scope', function ($scope) 
 			slideout.toggle();
 		}
 	});
-	
-	
-	//animateMe function 
+
+	//animateMe function
 	function animateMe(myObject, animateType, duration){
 		$(myObject).addClass("animated " + animateType).css("animation-delay", duration + "s");
 	}
-	
+
 	//we add the fadeIn animation with 0.1 seconds, for more information animate css, please google animate.css in your browser, there are varieties of animations available.
 	animateMe("#box-content", "fadeIn", 0.1);
+
+    if(currentRoute == $('#menu-list > li:last').find("a").attr('href').substring(2)) {
+        $('#nextPage').attr("disabled", true);
+    } else {
+        $('#nextPage').attr("disabled", false);
+    }
+
+    if(currentRoute == $('#menu-list > li:first').find("a").attr('href').substring(2) || currentRoute == '' ) {
+            $('#prevPage').attr("disabled", true);
+        } else {
+            $('#prevPage').attr("disabled", false);
+        }
 }]); 
 
+bytutorialEbookApp.run(function ($rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (e, current, pre) {
+        currentRoute = current.$$route.originalPath.substring(1);
+        $('#menu-list > li').each(function () {
+            //console.log(this.children[0].innerHTML.replace(/\s/g, '').toLowerCase())
+            if(this.children[0].innerHTML.replace(/\s/g, '').toLowerCase() == currentRoute) {
+                this.classList.add("highlight");
+            } else {
+                this.classList.remove("highlight");
+            }
+        });
+    });
+});
+
+$("#nextPage").on("click", function(){
+    if(currentRoute == '') currentRoute = 'chapter1'
+    console.log($('a[href$="#/'+currentRoute+'"]:first').parent().next("li").find("a").attr('href'))
+    if(typeof $('a[href$="#/'+currentRoute+'"]:first').parent().next("li").find("a").attr('href') !== "undefined") {
+        window.location.href = $('a[href$="#/'+currentRoute+'"]:first').parent().next("li").find("a").attr('href');
+        window.scrollTo(0,0);
+    }
+});
+
+$("#prevPage").on("click", function(){
+    console.log($('a[href$="#/'+currentRoute+'"]:first').parent().prev("li").find("a").attr('href'))
+    if(typeof $('a[href$="#/'+currentRoute+'"]:first').parent().prev("li").find("a").attr('href') !== "undefined") {
+        window.location.href = $('a[href$="#/'+currentRoute+'"]:first').parent().prev("li").find("a").attr('href');
+        window.scrollTo(0,0);
+    }
+});
